f0bf870c281d9330ee3cc6698d981cab13b4d0b5
sheeppocket <joonwon0201@gmail.com>
2026-08-26 15:16:14 +0800

    added mark and unmark function, with Task Class

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
  */
 public class Yapper {
     private static final String SEPARATOR = "____________________________________________________________";
+    private static final int MAX_TASKS = 100;
 
     public static void main(String[] args) {
         String banner = "__   __                               \n"
@@ -21,11 +22,36 @@ public class Yapper {
         System.out.println(SEPARATOR);
 
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
-        while (!command.equals("bye")) {
-            System.out.println(" " + command);
+        Task[] tasks = new Task[MAX_TASKS];
+        int taskCount = 0;
+
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+
+            if (command.equals("list")) {
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
+                }
+            } else if (command.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(command.substring(5)) - 1;
+                tasks[taskIndex].markAsDone();
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[taskIndex]);
+            } else if (command.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(command.substring(7)) - 1;
+                tasks[taskIndex].markAsNotDone();
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[taskIndex]);
+            } else {
+                tasks[taskCount] = new Task(command);
+                taskCount++;
+                System.out.println(" added: " + command);
+            }
             System.out.println(SEPARATOR);
-            command = scanner.nextLine();
         }
 
         System.out.println(" Bye. Hope to see you again soon!");
